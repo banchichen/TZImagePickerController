1883e4eb277acd5c61d1a3a59c402ad2745da055
Chren <aren372@126.com>
2016-10-11 10:20:38 +0800

    授权后刷新
    iOS10下首次进相册授权通过后应该刷新下当前页面 不然一直空白 需要退出重新进一次才能显示

--- TZImagePickerController/TZImagePickerController/TZImagePickerController.m
+++ TZImagePickerController/TZImagePickerController/TZImagePickerController.m
@@ -200,6 +200,10 @@
         [_settingBtn removeFromSuperview];
         [_timer invalidate];
         _timer = nil;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+           [self pushPhotoPickerVc];
+        });
     }
 }
 
