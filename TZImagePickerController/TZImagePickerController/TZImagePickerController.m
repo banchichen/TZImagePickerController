aeb288f54cbfaf0519d52a576ad123150b2f934f
yuzheng.tz <yuzheng.tz@alibaba-inc.com>
2017-11-23 10:27:14 +0800

    发布1.9.6版本：优化视频预览和gif预览页toolbar在iPhoneX上的样式

--- TZImagePickerController/TZImagePickerController/TZImagePickerController.m
+++ TZImagePickerController/TZImagePickerController/TZImagePickerController.m
@@ -4,7 +4,7 @@
 //
 //  Created by 谭真 on 15/12/24.
 //  Copyright © 2015年 谭真. All rights reserved.
-//  version 1.9.5 - 2017.11.15
+//  version 1.9.6 - 2017.11.23
 //  更多信息，请前往项目的github地址：https://github.com/banchichen/TZImagePickerController
 
 #import "TZImagePickerController.h"
