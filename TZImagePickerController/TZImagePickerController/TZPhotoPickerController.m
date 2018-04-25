37dc1af9d5274077e6cf27e71a8ca6e3dce38758
yuzheng.tz <yuzheng.tz@alibaba-inc.com>
2018-04-25 13:08:43 +0800

    修复单选视频时视频拍摄回调不走的问题，发布2.1.0.2版本

--- TZImagePickerController/TZImagePickerController/TZPhotoPickerController.m
+++ TZImagePickerController/TZImagePickerController/TZPhotoPickerController.m
@@ -418,6 +418,16 @@ static CGFloat itemMargin = 5;
 
 - (void)callDelegateMethodWithPhotos:(NSArray *)photos assets:(NSArray *)assets infoArr:(NSArray *)infoArr {
     TZImagePickerController *tzImagePickerVc = (TZImagePickerController *)self.navigationController;
+    if (!tzImagePickerVc.allowPickingImage && tzImagePickerVc.allowPickingVideo && tzImagePickerVc.maxImagesCount == 1) {
+        if ([tzImagePickerVc.pickerDelegate respondsToSelector:@selector(imagePickerController:didFinishPickingVideo:sourceAssets:)]) {
+            [tzImagePickerVc.pickerDelegate imagePickerController:tzImagePickerVc didFinishPickingVideo:[photos firstObject] sourceAssets:[assets firstObject]];
+        }
+        if (tzImagePickerVc.didFinishPickingVideoHandle) {
+            tzImagePickerVc.didFinishPickingVideoHandle([photos firstObject], [assets firstObject]);
+        }
+        return;
+    }
+    
     if ([tzImagePickerVc.pickerDelegate respondsToSelector:@selector(imagePickerController:didFinishPickingPhotos:sourceAssets:isSelectOriginalPhoto:)]) {
         [tzImagePickerVc.pickerDelegate imagePickerController:tzImagePickerVc didFinishPickingPhotos:photos sourceAssets:assets isSelectOriginalPhoto:_isSelectOriginalPhoto];
     }
