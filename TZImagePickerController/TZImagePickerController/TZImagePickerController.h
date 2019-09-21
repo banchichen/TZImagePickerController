9ed9f22bcb82158037cdd62a0f93e6196d163530
yuzheng <yuzheng.tz@alibaba-inc.com>
2019-09-21 18:33:07 +0800

    release 3.2.4

--- TZImagePickerController/TZImagePickerController/TZImagePickerController.h
+++ TZImagePickerController/TZImagePickerController/TZImagePickerController.h
@@ -4,7 +4,7 @@
 //
 //  Created by 谭真 on 15/12/24.
 //  Copyright © 2015年 谭真. All rights reserved.
-//  version 3.2.3 - 2019.09.04
+//  version 3.2.4 - 2019.09.21
 //  更多信息，请前往项目的github地址：https://github.com/banchichen/TZImagePickerController
 
 /*
