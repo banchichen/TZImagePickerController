6fb0d3cf1e6fe37c6fe497df53467f219f17205e
xiaoxiao <xx2662118@alibaba-inc.com>
2026-02-05 20:52:48 +0800

    当 (insertedCount == 0 && removedCount == 0 && changedCount > 1) 时，直接退出return 。不进行刷新

--- TZImagePickerController/TZImagePickerController/TZPhotoPickerController.m
+++ TZImagePickerController/TZImagePickerController/TZPhotoPickerController.m
@@ -1144,7 +1144,9 @@ static CGFloat itemMargin = 5;
             NSInteger insertedCount = changeDetail.insertedObjects.count;
             NSInteger removedCount = changeDetail.removedObjects.count;
             NSInteger changedCount = changeDetail.changedObjects.count;
-            if (insertedCount > 0 || removedCount > 0 || changedCount > 0) {
+            if (insertedCount == 0 && removedCount == 0 && changedCount > 1) {
+                return;
+            } else if (insertedCount > 0 || removedCount > 0 || changedCount > 0) {
                 self.model.result = changeDetail.fetchResultAfterChanges;
                 self.model.count = changeDetail.fetchResultAfterChanges.count;
                 [self fetchAssetModels];
